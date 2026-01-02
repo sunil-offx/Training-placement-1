e3cb52875065f94d9ca3279294f497ab2374de9f
sunil-offx <sunilkumarsra2@gmail.com>
2026-01-02 10:15:16 +0530

    Delete 01-02-2025 directory

--- 01-02-2025/Reverse string.py
+++
@@ -1,8 +0,0 @@
-# Function to reverse a string
-def reverse_string(s):
-    return s[::-1]
-
-# Example usage
-input_string = "hello"
-reversed_string = reverse_string(input_string)
-print("Reversed string:", reversed_string)
